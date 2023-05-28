cdcff99705bc5a72f2566ae0ba22bcb5ed6747e4
suneel <sikarvarsunil@gmail.com>
2023-05-28 21:46:28 +0530

    add web soket method and add subscribe all token and modified the underlying and directive data with added price and status key

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,16 +1,43 @@
 
 import React, { useEffect, useState } from 'react';
 import { RequestHandler } from '../clients/';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 const DataTable = () => {
+    const [socketUrl, setSocketUrl] = useState('wss://prototype.sbulltech.com/api/ws');
+    const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl, 
+        {
+            shouldReconnect: (closeEvent) => true,
+            reconnectAttempts: 10,
+            reconnectInterval: 3000,
+          });
     const [ status, setStatus] = useState(false);
     const [ underlyingsData, setUnderlyingsData] = useState([]);
 
     const getUnderlyingsData = async (url='/underlyings') => {
         
+  
         const res = await RequestHandler(url, {});
-            setUnderlyingsData(res.data.payload)
+        const getTokensList = getTokens(res.data.payload);
+        getSubscribe(getTokensList, true)
+        setUnderlyingsData(res.data.payload)
         
+    }
+
+    const getSubscribe = (tokenData, isSubscribe) => {
+        let req = {
+            "msg_command":`${isSubscribe ? "subscribe" : "unsubscribe"}`,
+            "data_type":"quote",
+            "tokens":tokenData
+           }
+           console.log('req', req)
+        sendMessage(JSON.stringify(req));
+    }
+
+    const getTokens = (tokens) => {
+        return tokens && tokens.reduce((acc, item) => {
+           return [...acc, item.token];
+        }, []);
     }
 
     const getDirectiveData = (token) => {
@@ -26,11 +53,36 @@ const DataTable = () => {
     useEffect(() => {
         const url = `/underlyings`
         getUnderlyingsData(url);
+        return () => {
+            if(underlyingsData) {
+                const getTokensList = getTokens(underlyingsData);
+                getSubscribe(getTokensList, false)
+            }
+        }
     }, [])
 
 
+    useEffect(() => {
+        let modiFied = underlyingsData;
+        if (lastMessage !== null) {
+            let payload = JSON.parse(lastMessage.data).payload;
+          modiFied.forEach((item) => {
+            
+            if(payload.token === item.token) {
+                let newPrice = payload.price ? payload.price.toFixed(2) : item.price ? item.price : '';
+                let status = item.price < newPrice ? 'up' : (item.price > newPrice ? 'down' : 'nochange');
+                item.price = newPrice;
+                item.status = status;
+            }
+          })
+          
+          setUnderlyingsData(modiFied)
+        }
+      }, [lastMessage, setUnderlyingsData]);
+
     return (
         <div>
+            
             <div><h1>{status ? 'Directive' : 'Underlyings'}</h1>
             {status && <button className='back' onClick={() => onBack(false)}>{`< Back `}</button>}
             <ul className='listbox' role='listbox'>
